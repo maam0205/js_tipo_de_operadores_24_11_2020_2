6ebd7e6667495b8e6335ad1db10d04aa132e3464
unknown <maalma0205@gmail.com>
2020-11-24 20:43:49 -0500

    operaciones basicas

---
+++ main.js
@@ -0,0 +1,47 @@
+//operadores de incremento
+/*let numero_1=10;
+let numero_2=20;
+numero_1 ++2;
+numero_1 +=2;
+console.log(numero_1)*/
+
+
+//operadores de incremento
+/*let numero_1=10;
+let numero_2=20;
+numero_1--;
+numero_1 -=2;
+console.log(numero_1);*/
+
+//operadores de Exponenciacion
+/*let numero_1=10;
+let numero_2=20;
+numero_1 **= numero_2;
+console.log(numero_1);*/
+
+//operadores de modulo segun el valor
+/*let numero_1=15;
+let numero_2=2;
+numero_1 %=numero_2;
+console.log(numero_1);*/
+
+//operadores basicas
+/*let numero_1=15;
+let numero_2=2;
+let resultado;
+resultado=numero_1+numero_2;
+console.log(resultado);
+resultado=numero_1-numero_2;
+console.log(resultado);
+resultado=numero_1*numero_2;
+console.log(resultado);
+resultado=numero_1/numero_2;
+console.log(resultado);
+resultado=numero_1%numero_2;
+console.log(resultado);
+resultado=numero_1**numero_2;
+console.log(resultado);
+resultado=Math.cbrt(numero_1,3);
+console.log(resultado);
+resultado=Math.sqrt(numero_1,2);
+console.log(resultado);*/
